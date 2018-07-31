00f2b32f771332171d96995ca999ea074523a58c
Filip Savić <filip.savic@trikoder.net>
2018-07-31 10:04:59 +0200

    Add matches with Opera vendor prefix for old versions

--- src/core.js
+++ src/core.js
@@ -141,7 +141,7 @@ var utils = {
 
     matches: function(el, selector) {
 
-        return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector).call(el, selector);
+        return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
 
     },
 
